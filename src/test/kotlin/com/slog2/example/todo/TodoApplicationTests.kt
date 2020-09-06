176b722c3f585851a1c42435ba71201a16ba61d6
Jaewon Lee <lee.jaewon@nbt.com>
2020-09-06 17:01:18 +0900

    first commit

---
+++ src/test/kotlin/com/slog2/example/todo/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slog2.example.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
